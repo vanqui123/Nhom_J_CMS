578d4a6a1ff1c7dd5f22fc9ca6b28378cfb7e9b9
vietnguyen <nqv171002@gmail.com>
2022-11-25 08:50:05 +0700

    job_post

--- wp-content/themes/jobscout/sections/jobposting.php
+++ wp-content/themes/jobscout/sections/jobposting.php
@@ -5,19 +5,21 @@
  * @package JobScout
  */
 
-$job_title         = get_theme_mod( 'job_posting_section_title', __( 'Job Posting', 'jobscout' ) );
+$job_title         = get_theme_mod( 'job_posting_section_title', __( 'TOP JOBS', 'jobscout' ) );
 $ed_jobposting     = get_theme_mod( 'ed_jobposting', true );
 $count_posts       = wp_count_posts('job_listing'); 
+$post_job_url    = get_theme_mod( 'post_job_url', true );
 if ( $ed_jobposting && jobscout_is_wp_job_manager_activated() && $job_title  ) {
     ?>
     <section id="job-posting-section" class="top-job-section">
         <div class="container">
-            <?php 
+            <?php
                 if( $job_title ) echo '<h2 class="section-title">'. esc_html( $job_title ) .'</h2>'; 
                 if( jobscout_is_wp_job_manager_activated() && $count_posts->publish != 0 ){ ?>
                     <div class="row">
                         <div class="col-md-12">
                             <?php echo do_shortcode('[jobs show_filters="false" post_status="publish"]'); ?>
+                            <div class="btn-wrap" ><a id="btn-job-view"  href="http://localhost:8080/CMS/wordpress/jobs/" class="btn">VIEW MORE JOBS</a></div>
                         </div>
                     </div>
                 <?php } 
